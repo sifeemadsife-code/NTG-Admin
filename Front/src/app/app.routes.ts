7a316d4a59eb543402b98480dd29632a7c56b95c
SaifEmad <saifemad@gmail.com>
2026-06-28 14:46:13 +0300

    Saif Emad _ Delete unused files

--- Front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { EngineerDetails } from './pages/engineer-details/engineer-details';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: EngineerDetails
-  }
-];
