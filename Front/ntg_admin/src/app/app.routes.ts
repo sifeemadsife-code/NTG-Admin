87a628e15049cdc42dd196e323d2455624280d92
SaifEmad <saifemad@gmail.com>
2026-06-29 00:08:33 +0300

    Saif emad

--- Front/ntg_admin/src/app/app.routes.ts
+++ Front/ntg_admin/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { EngineerDetailsOverView } from './Components/engineer-details-overview/engineer-details';
 import { ngineerDetails } from './Components/engineer-details-documents/engineer-details';
 import { NotFound } from './Components/not-found/not-found';
+import { Login } from './Components/login/login';
 
 export const routes: Routes = [
     {
         path : "",
-        component : EngineerDetailsOverView
+        component : Login
     },
     {
         path : "engineerDetails",
