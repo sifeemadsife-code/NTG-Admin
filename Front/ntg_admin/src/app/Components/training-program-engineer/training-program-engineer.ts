7a55e1655ff5a217c111c613b90238ddc3a3b575
SaifEmad <saifemad@gmail.com>
2026-07-05 02:31:41 +0300

    Saif Emad

--- Front/ntg_admin/src/app/Components/training-program-engineer/training-program-engineer.ts
+++ Front/ntg_admin/src/app/Components/training-program-engineer/training-program-engineer.ts
@@ -2,23 +2,32 @@ import { Component, computed, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingService } from '../../Services/training-service';
 import { Training } from '../../Models/Training';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { EngineerCards } from '../../Models/engineer-cards';
-import { StudentsListInterface } from '../../Models/Students_list';
-import { Student } from '../../Services/student';
+import { EngineerService } from '../../Services/engineer';
+import { Engineer } from '../../Models/engineer';
 
 @Component({
   selector: 'app-training-program-students',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './training-program-engineer.html',
-  styleUrls: ['./training-program-engineer.css']
+  styleUrls: ['./training-program-engineer.css'],
 })
 export class TrainingProgramEngineer implements OnInit {
-program!: Training;
   loading = true;
 
-  constructor(private service: TrainingService) {}
+  engineer = signal<Engineer>({
+    id: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    education: '',
+    employmentHistory: '',
+    numberOfYearsOfExperience: 0,
+  });
+
   training = signal<Training>({
     id: 1,
     teacherId: 1,
@@ -32,24 +41,60 @@ program!: Training;
     createdAt: new Date('0-0-0'),
     totalStudents: 0,
   });
+
   cards = signal<EngineerCards>({
     students: 0,
     reports: 0,
     rating: 0,
   });
+
   totalStudents = signal<number>(0);
+
   studentsPercentage = computed(() => {
     const total = this.totalStudents();
     if (!total) return 0;
     return (this.cards().students / total) * 100;
   });
+
+  program_id = 0;
+
+  constructor(
+    private service: TrainingService,
+    private engineerService: EngineerService,
+    private router: ActivatedRoute,
+  ) {}
+
   ngOnInit() {
-    this.service.getProgram(this.training().id).subscribe({
+    this.program_id = Number(this.router.snapshot.paramMap.get('id'));
+
+    this.service.getProgram(this.program_id).subscribe({
       next: (data) => {
-        (this.training.set(data), console.log(data));
+        this.training.set(data);
+        this.loading = false;
+        console.log(data);
+
+        this.engineerService.getEngineer(data.teacherId).subscribe({
+          next: (engineerData) => {
+            this.engineer.set(engineerData);
+            console.log(engineerData);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+
+        this.engineerService.getEngineerCards(data.teacherId).subscribe({
+          next: (cardsData) => {
+            this.cards.set(cardsData);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
       },
       error: (err) => {
         console.log(err);
+        this.loading = false;
       },
     });
   }
