bb1a55b5a275b0b34f9b331e2685d7ce5dbae65b
El_lool <aliahmed99199@gmail.com>
2026-07-05 01:55:54 +0300

    El lool

--- Front/ntg_admin/src/app/app.routes.ts
+++ Front/ntg_admin/src/app/app.routes.ts
@@ -9,10 +9,11 @@ import { StudentsList } from './Components/students-list/students-list';
 import { TrainingProgramOverveiw } from './Components/training-program-overveiw/training-program-overveiw';
 import { ProgramsList } from './Components/programs-list/programs-list';
 import { TrainingProgramStudents } from './Components/training-program-students/training-program-students';
+import { TrainingProgramEngineer } from './Components/training-program-engineer/training-program-engineer';
 export const routes: Routes = [
   {
     path: '',
-    component: TrainingProgramStudents,
+    component: TrainingProgramEngineer,
     },
   {
     path: 'engineers/:id',
